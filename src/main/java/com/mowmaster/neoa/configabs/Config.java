907526bf891ca8aa46cd26aae9b6a4dc82976da5
Mowmaster <Mowmaster@users.noreply.github.com>
2016-12-28 05:02:00 -0600

    Update Config.java
    Added in some configs for Vanilla tweaks. Will try to make most tweaks configurable in the future.

--- src/main/java/com/mowmaster/neoa/configabs/Config.java
+++ src/main/java/com/mowmaster/neoa/configabs/Config.java
@@ -1,7 +1,42 @@
 package com.mowmaster.neoa.configabs;
 
-/**
- * Created by KingMowmaster on 12/27/2016.
- */
-public class Config {
+import net.minecraftforge.common.config.Configuration;
+import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+
+import java.io.File;
+
+
+public class Config 
+{
+  
+  public static Configuration configFile;
+  
+  
+  
+  public static boolean RecyclableGlass;
+  public static boolean FlintForDays;
+  public static boolean LeatherForDays;
+  
+  
+  
+  public static void initCongif(File file)
+  {
+    configFile = new Configuration(file);
+    syncConfig();
+  }
+  
+  public static void syncConfig()
+  {
+    String category;
+    
+    catagory = "Vanilla Tweaks";
+    configFile.addCustomCatagoryComment(catagory, "Added by NEoA");
+    
+    RecyclableGlass = configFile.getBoolean("Recyclable Glass", catagory, true, "Do you want to enable the crafting and smelting of glass dust?");
+    FlintForDays = configFile.getBoolean("Flint for Days", catagory, true, "Should two gravel amke flint?");
+    LeatherForDays = configFile.getBoolean("Leather for Days", catagory, true, "Can leather be smelted from flesh?");
+    
+  }
+  
 }
