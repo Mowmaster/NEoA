4aad32deaa7d969c668edccb228d209629a6ab31
Mowmaster <Mowmaster@users.noreply.github.com>
2016-12-28 05:45:18 -0600

    Created recipes folder and vTweaks with configs!
    Added recipes folder
    Added configurable recipes for flint, 2 gravel horizontally or vertically will make one flint
    Added rotten flesh to be smelted into leather, also gives a tiny bit of exp.

---
+++ src/main/java/com/mowmaster/neoa/recipes/vanillaTweaks.java
@@ -0,0 +1,25 @@
+package com.mowmaster.neoa.recipes;
+
+import com.mowmaster.neoa.Configs.config;
+
+
+
+
+public class vanillaTweaks
+{
+  public static void vT()
+  {
+    if (config.FlintForDays)
+    {
+      // Craft Gravel into Flint
+      GameRegistry.addShapedRecipe(new ItemStack(Items.FLINT,1), new Object[]{"XX", 'X', new ItemStack(Blocks.GRAVEL)});
+      GameRegistry.addShapedRecipe(new ItemStack(Items.FLINT,1), new Object[]{"X","X", 'X', new ItemStack(Blocks.GRAVEL)});
+    }
+    
+    if (config.LeatherForDays)
+    {
+      GameRegistry.addSmelting(new ItemStack(Items.ROTTEN_FLESH),new ItemStack(Items.Leather,1), 0.1F);
+    }
+    
+  }
+}
