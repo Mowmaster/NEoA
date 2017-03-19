88e210600113be3d8b944065900261f073c6a7b8
Mowmaster <mowmaster@outlook.com>
2017-03-18 23:27:08 -0500

    Fixed minor issues, however fluid textures do no render for some reason, will put a release out and fix later

--- src/main/java/com/mowmaster/neoa/fluids/Fluids.java
+++ src/main/java/com/mowmaster/neoa/fluids/Fluids.java
@@ -25,31 +25,31 @@ public class Fluids {
 
     public static void regfluids()
     {
-        fluidCake = new Fluid("cake", new ResourceLocation("dust:blocks/cake_still"), new ResourceLocation("dust:blocks/cake_flowing")).setDensity(2400).setViscosity(4000);
+        fluidCake = new Fluid("cake", new ResourceLocation("neweraofadventure:blocks/cake_still"), new ResourceLocation("neweraofadventure:blocks/cake_flowing")).setDensity(2400).setViscosity(4000);
         FluidRegistry.registerFluid(fluidCake);
 
 
-        fluidPie = new Fluid("pie", new ResourceLocation("dust:blocks/pie_still"), new ResourceLocation("dust:blocks/pie_flowing")).setDensity(2400).setViscosity(4000);
+        fluidPie = new Fluid("pie", new ResourceLocation("neweraofadventure:blocks/pie_still"), new ResourceLocation("neweraofadventure:blocks/pie_flowing")).setDensity(2400).setViscosity(4000);
         FluidRegistry.registerFluid(fluidPie);
 
 
-        fluidCookie = new Fluid("cookie", new ResourceLocation("dust:blocks/cookie_still"), new ResourceLocation("dust:blocks/cookie_flowing")).setDensity(2400).setViscosity(4000);
+        fluidCookie = new Fluid("cookie", new ResourceLocation("neweraofadventure:blocks/cookie_still"), new ResourceLocation("neweraofadventure:blocks/cookie_flowing")).setDensity(2400).setViscosity(4000);
         FluidRegistry.registerFluid(fluidCookie);
 
 
-        fluidBread = new Fluid("bread", new ResourceLocation("dust:blocks/bread_still"), new ResourceLocation("dust:blocks/bread_flowing")).setDensity(2400).setViscosity(4000);
+        fluidBread = new Fluid("bread", new ResourceLocation("neweraofadventure:blocks/bread_still"), new ResourceLocation("neweraofadventure:blocks/bread_flowing")).setDensity(2400).setViscosity(4000);
         FluidRegistry.registerFluid(fluidBread);
 
 
-        fluidMSoup = new Fluid("msoup", new ResourceLocation("dust:blocks/msoup_still"), new ResourceLocation("dust:blocks/msoup_flowing")).setDensity(2400).setViscosity(4000);
+        fluidMSoup = new Fluid("msoup", new ResourceLocation("neweraofadventure:blocks/msoup_still"), new ResourceLocation("neweraofadventure:blocks/msoup_flowing")).setDensity(2400).setViscosity(4000);
         FluidRegistry.registerFluid(fluidMSoup);
 
 
-        fluidRSoup = new Fluid("rsoup", new ResourceLocation("dust:blocks/rsoup_still"), new ResourceLocation("dust:blocks/rsoup_flowing")).setDensity(2400).setViscosity(4000);
+        fluidRSoup = new Fluid("rsoup", new ResourceLocation("neweraofadventure:blocks/rsoup_still"), new ResourceLocation("neweraofadventure:blocks/rsoup_flowing")).setDensity(2400).setViscosity(4000);
         FluidRegistry.registerFluid(fluidRSoup);
 
 
-        fluidBSoup = new Fluid("bsoup", new ResourceLocation("dust:blocks/bsoup_still"), new ResourceLocation("dust:blocks/bsoup_flowing")).setDensity(2400).setViscosity(4000);
+        fluidBSoup = new Fluid("bsoup", new ResourceLocation("neweraofadventure:blocks/bsoup_still"), new ResourceLocation("neweraofadventure:blocks/bsoup_flowing")).setDensity(2400).setViscosity(4000);
         FluidRegistry.registerFluid(fluidBSoup);
 
 
